ac4e9430a11dd7bf622bb2af893f62abcd0d9867
SeanMoon <deccj97@naver.com>
2021-10-05 20:21:15 +0900

    4th week commit

--- Assets/Scenes/cChar.cs
+++ Assets/Scenes/cChar.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 
 public class cChar : MonoBehaviour {
+    static private cChar getChar;
+    static public cChar I {
+        get {
+            if (getChar == null)
+                return null;
+            return getChar;
+        }
+    }    
     GameObject cGameObject;
     int vDirection;
     int vAnimationIndex;
     int vLifePoint;
     float vRunSpeed;
     float vJumpSpeed;
-
-    public float VRunSpeed { get => vRunSpeed; set => vRunSpeed = value; }
+    //public float VRunSpeed { get => vRunSpeed; set => vRunSpeed = value; } //get set 학습 필요
+    
 }
--- Assets/Scenes/cGV.cs
+++ Assets/Scenes/cGV.cs
@@ -1,12 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 
-public abstract class cGV
+public class cGV
 {
-
-    void Update()
-    {
-        
+    static private cGV getGV;
+    static public cGV I { 
+        get {
+            if (getGV != null)//기존에 만들었으면 전달
+                return getGV;
+            getGV = new cGV();//기존에 안만들었으면 생성 후 전달
+            return getGV; 
+        } 
     }
+
+    public const int vFPS = 60;
+    public void QuitProcess(string tstring) {
+        #if UNITY_EDITOR
+        Debug.LogError(tstring);
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    public void SetAnimation(int tAnimationIndex, ref cChar tCharacter) {
+
+    }
+
 }
