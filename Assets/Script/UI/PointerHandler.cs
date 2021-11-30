b421907b2fe1ee0491ee8c0bd768c216f1d7272a
SeanMoon <deccj97@naver.com>
2021-12-01 01:57:29 +0900

    fix PointerHandler

--- Assets/Script/UI/PointerHandler.cs
+++ Assets/Script/UI/PointerHandler.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PointerHandler : MonoBehaviour
+public class PointerHandler : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
 {
     public void OnPointerDown(PointerEventData tEventData) {
-        if()
+        if(cGV.I.sUIButton.cUIButtonGameObject[cGV.UI_MOVE_LEFT] == gameObject) {
+            cGV.I.vCheckMoveLeft = true;
+        }else if(cGV.I.sUIButton.cUIButtonGameObject[cGV.UI_MOVE_RIGHT] == gameObject) {
+            cGV.I.vCheckMoveRight = true;
+        }
+    }
+    public void OnPointerUp(PointerEventData tEventData) {
+        if (cGV.I.sUIButton.cUIButtonGameObject[cGV.UI_MOVE_LEFT] == gameObject) {
+            cGV.I.vCheckMoveLeft = false;
+        } else if (cGV.I.sUIButton.cUIButtonGameObject[cGV.UI_MOVE_RIGHT] == gameObject) {
+            cGV.I.vCheckMoveRight = false;
+        }
     }
 }
